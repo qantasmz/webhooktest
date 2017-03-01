34097b1df6dd4884a30b0fdff97e2ff4edb7a41a
qanta <qanta@micd.com>
2017-02-28 22:22:01 -0500

    init

---
+++ test.php
@@ -0,0 +1,9 @@
+<?php
+
+$arr = Array();
+$arr['speech'] = 'I am fucking Qanta Shimizu';
+$arr['source'] = 'webhook-sample';
+$arr['displayText'] = 'I am fucking Qanta Shimizu';
+$json = json_encode($arr);
+echo $json;
+?>
